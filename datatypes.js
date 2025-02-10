075950e5e2a3e7273ada35d95d5d20508c03aafa
Payal <bharadwajpayal143@gmail.com>
2025-02-10 15:59:25 +0530

    added database.js file

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+/*Dataypes in javscript
+**Primitive datatypes
+1. Number
+2. Bigint
+3. Boolean => True/False
+4. String = " "
+5. Undefined
+6. Null => standalone value
+7. Symbol => unique
+
+** non primitive
+8. Object
+*/
+
+let x = 3 
+console.log(typeof(x)) // number
+let y = BigInt("123456789012345678901234567890");
+console.log(typeof(y)) //bigint
+//boolean 
+let a =5
+let b =6
+let c =5
+console.log(a==b) //false
+console.log(b==c) //false
+console.log(a==c) //true
+
