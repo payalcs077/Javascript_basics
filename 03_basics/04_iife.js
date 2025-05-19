39744614d2ce46ecf1840ac93d94c24236f0fead
Payal <bharadwajpayal143@gmail.com>
2025-05-19 18:29:39 +0530

    added iife.js file

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+( ()=>{
+    console.log(`DB CONNECTED`);
+})();
+
+( function chai(){
+        console.log(`DB CONNECTED TWO`);
+        
+})();
+
+((name)=>{
+  console.log(`DB CONNECTED THREE ${name}`);
+})("Payal")
