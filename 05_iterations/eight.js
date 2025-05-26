44ceb0a3eed9898853c6675259fba5826062d04b
Payal <bharadwajpayal143@gmail.com>
2025-05-26 19:11:31 +0530

    added reduce method in eight.js file

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -4,3 +4,24 @@ const myNum = [1,2,3]
 
 const mytotal = myNum.reduce((acc,curr)=> acc + curr,0)
 console.log(mytotal);
+
+
+const shoppingCart = [
+    {
+        itemName : "Javascript course",
+        itemPrice : 2999
+    },
+    {
+        itemName : "Python course",
+        itemPrice : 999
+    },
+    {
+        itemName : "Datascience",
+        itemPrice : 12999
+    }
+
+]
+
+const priceToPay = shoppingCart.reduce((acc,item) => acc + item.itemPrice, 0)
+
+console.log(priceToPay);
