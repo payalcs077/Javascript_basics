591b132b00e0de293480a016c5bf47b024c2bba1
Payal <bharadwajpayal143@gmail.com>
2025-05-26 12:11:30 +0530

    added two.js file

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// let index = 0;
+
+// while(index <= 10){
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+const myarray = ['Ariel', 'Cinderlla', 'Snowwhite']
+let i = 0;
+while(i < myarray.length){
+    //console.log(`Princess ${myarray[i]}`);
+    i = i + 1
+}
+
+// let score = 0;
+let score = 11
+
+do {
+    console.log(`value of score ${score}`);
+    score = score + 1
+} while (score <= 10);
